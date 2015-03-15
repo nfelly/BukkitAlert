44caad3d8dfc9f07eb453e00283610070f58e026
nfelly <nfell2009@outlook.com>
2015-03-15 20:59:23 +0000

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,5 +1,4 @@
-var url = "http://bukkit.org/";
-var dbo_url = "http://dev.bukkit.org/home/";
+var url = "http://spigotmc.org/";
 
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.create({'url': localStorage["badge_link"]}, function(tab) {});
@@ -15,26 +14,22 @@ var total = 0;
 localStorage["time_length"] = 30000;
 localStorage["sound_play"] = "true";
 localStorage["auto_close"] = 60000;
-localStorage["badge_link"] = "http://bukkit.org";
+localStorage["badge_link"] = "http://spigotmc.org";
 
 getAlerts();
-
-function getYQLAddress(apikey) {
-    return "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fdev.bukkit.org%2Fhome%2Fprivate-messages%2F%3Fapi-key%3D" + apikey + "%22&format=json&callback=cbfunc";
-}
 
 function getAlerts() {
     console.log("Getting alerts...");
 
     total = 0;
 
-    console.log("Connecting to Bukkit...");
+    console.log("Connecting to Spigot...");
     var xhr = new XMLHttpRequest();
     xhr.open( 'GET', url + '.json', true );
     xhr.onload = function () {
         var alerts = parseInt(window.JSON.parse( xhr.responseText )._visitor_alertsUnread);
         var conversations = parseInt(window.JSON.parse( xhr.responseText )._visitor_conversationsUnread);
-        console.log("Connected to Bukkit. Alerts: " + alerts + ", Conversations: " + conversations + ".");
+        console.log("Connected to Spigot. Alerts: " + alerts + ", Conversations: " + conversations + ".");
         
         if(alerts > alerts_old) {
             console.log("Incoming alert notification!");
@@ -52,34 +47,10 @@ function getAlerts() {
         }
     };
     xhr.onerror = function () {
-        console.log("An error occurred while connecting to Bukkit!");
+        console.log("An error occurred while connecting to Spigot!");
         chrome.browserAction.setBadgeText({text: "Error"});
     };
     xhr.send();
-    
-    var dbo_key = localStorage["dbo_key"];
-    if (!(dbo_key === "Undefined" || dbo_key === "")) {
-        console.log("Connecting to Bukkit Dev...");
-        var dbo_xhr = new XMLHttpRequest();
-        dbo_xhr.open('GET', getYQLAddress(dbo_key), true);
-        dbo_xhr.onload = function() {
-            var pms = (dbo_xhr.responseText.split("New messages").length - 1) / 2;
-            console.log("Connected to Bukkit Dev. PMs: " + pms + ".");
-            
-            if(pms > pms_old) {
-                console.log("Incoming PM notification!");
-                notify(pms, "Dev PM" + (pms == 1 ? "" : "s"), dbo_url + "private-messages");
-                pms_old = pms;
-                total += pms;
-                updateBadge();
-            }
-        };
-        dbo_xhr.onerror = function() {
-            console.log("An error occurred while connecting to Bukkit Dev!");
-            chrome.browserAction.setBadgeText({text: "Error"});
-        };
-        dbo_xhr.send();
-    }
     
     setTimeout(getAlerts, localStorage["time_length"]);
 }
@@ -93,7 +64,7 @@ function notify(number, type, url) {
     var notification = webkitNotifications.createNotification(
         'img/48.png',
         'Attention:',
-        'You have ' + number + ' unread Bukkit ' + type + '!'
+        'You have ' + number + ' unread Spigot ' + type + '!'
     );
     notification.show();
     playSound();
